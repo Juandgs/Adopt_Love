f25e7b34e8a613e9704b192298ecc3350f078756
Miguel Rabih <rabihmiguel@gmail.com>
2025-04-03 12:49:05 -0500

    Guardando rama antes de fucionarlas

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
 
 Route::get('/home', function () {
     return view('home');
